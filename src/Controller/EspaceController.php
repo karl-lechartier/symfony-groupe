6cd61f939eb742330e25345bb9764462b949b690
doxka <karllechartier2@gmail.com>
2022-12-12 10:27:07 +0100

    ajouté pt

--- src/Controller/EspaceController.php
+++ src/Controller/EspaceController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\EspaceType;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Espace;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +20,25 @@ class EspaceController extends AbstractController
         
         return $this->render('espace/index.html.twig', [
             'espaces' => $espaces,
+        ]);
+    }
+
+    #[Route('/espace/ajouter', name: 'app_espace_ajouter')]
+    public function ajouter(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $espace = new Espace();
+        $form = $this->createForm(EspaceType::class,$espace);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $doctrine->getManager();
+            $em->persist($espace);
+            $em->flush();
+            return $this->redirectToRoute("app_espace");
+        }
+
+        return $this->render('espace/ajouter.html.twig', [
+            'formulaire' => $form->createView()
         ]);
     }
 }
