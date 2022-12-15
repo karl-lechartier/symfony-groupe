4ef2162c1da6c611e453cb9dd4581e70df43aca7
doxka <karllechartier2@gmail.com>
2022-12-15 13:29:44 +0100

    menu réparé

--- src/Controller/EncloController.php
+++ src/Controller/EncloController.php
@@ -40,8 +40,9 @@ class EncloController extends AbstractController
             $em = $doctrine->getManager();
             $em->persist($enclo);
             $em->flush();
-            return $this->redirectToRoute("app_espace");
+            return $this->redirectToRoute("app_enclo_voir", ["id" => $enclo->getEspaceID()->getId()]);
         }
+
 
         return $this->render('enclo/ajouter.html.twig', [
             'formulaire' => $form->createView()
